a3d504b8e9c6f0c5a3220cc3a62fb7cc49845131
Alex Griffen <radhack242@gmail.com>
2018-03-13 15:19:58 -0700

    Update main.js
    add comments for client_id and sign_url

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ import HelloSignEmbedded from './HelloSignEmbedded';
 
 ReactDOM.render(
     <HelloSignEmbedded 
-        clientId="client-id-123456789"
+        clientId="client-id-123456789" // update with your client_id
         uxVersion={2}
-        url="https://app.hellosign.com/editor/embeddedSign?signature_id=78caf2a1d01cd39cea2bc1cbb340dac3&token=b6b8e7deaf8f0b95c029dca049356d4a2cf9710a"
+        url="https://app.hellosign.com/editor/embeddedSign?signature_id=78caf2a1d01cd39cea2bc1cbb340dac3&token=b6b8e7deaf8f0b95c029dca049356d4a2cf9710a" //update with your sign_url
         inContainer={true} 
     />,
     document.getElementById('root')
